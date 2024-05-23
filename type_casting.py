43addd7bfd1bd3b086c8d2123aafa10ca5d19c44
Dhruvbindal288 <dhruvbindal288@gmail.com>
2024-05-23 11:49:15 +0530

    first

---
+++ type_casting.py
@@ -0,0 +1,16 @@
+a=3
+b=float(a)
+print(b)
+print(type(b))
+
+
+
+c={1,2,3,4,5,6}
+d=tuple(c)
+print(type(d))
+
+
+list_of_tuples = [("name", "Alice"), ("age", 30)]
+dict_value = dict(list_of_tuples)
+print(dict_value)  
+print(type(dict_value))  
