5350fb4c0b49befa7ce00664acede4977ae801de
Dhruvbindal288 <dhruvbindal288@gmail.com>
2024-05-26 18:05:47 +0530

    Practise of flow Control Statement

--- flow_control.py
+++ flow_control.py
@@ -1,4 +1,24 @@
-if 5>2:
-   print('Greater')
-else: 
-   print('Smaller')   
+a= int(input("Enter First number"))
+b=int(input("Enter Second number"))
+
+if a>=b:
+    if a==b:
+        print('a and b are equal')
+    else:
+        print('Number are not equal')
+else:
+    print("a is smaller than b")
+    
+    
+    
+    
+    
+day=input("Enter day: ") 
+if day=="Monday":
+    print("Today is Monday")
+elif day=="tuesday":
+       print("Today is tuesday")
+elif day=="Wednesday":
+       print("Today is Wednesday")
+else:
+    print("toad is Holiday")       
