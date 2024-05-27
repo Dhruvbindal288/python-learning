d40bb5c31c45d03c3ba3c128f1e33a8ff3a77602
Dhruvbindal288 <dhruvbindal288@gmail.com>
2024-05-27 17:45:49 +0530

    More Patterns

--- patterns.py
+++ patterns.py
@@ -2,3 +2,25 @@ for i in range(8):                  #rows
     for j in range(8):               #column
         print("*",end=" ")
     print(" ")    
+    
+    
+    
+    
+    
+print("TRIANGLE PATTERN")    
+
+rows = 5  
+
+for i in range(rows):           
+    for j in range(i + 1):      
+        print("*", end=" ")     
+    print("") 
+    
+    
+print("INVERTED TRIANGLE") 
+
+rows = 6  
+
+for i in range(rows):             
+    print(" " * (rows - i - 1), end="") 
+    print("*" * (i + 1))      
